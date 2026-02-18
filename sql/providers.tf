20e7d90124d4478d49c49219170d75d35626a08a
nessaoana <nessaoana09@gmail.com>
2026-02-18 13:01:02 -0300

    feat: update AWS provider configuration to use region us-east-1 and add endpoint settings

--- sql/providers.tf
+++ sql/providers.tf
@@ -7,8 +7,19 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-2"
-}
+  region = "us-east-1"
 
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2            = "http://localhost:4566"
+    eks            = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+  }
+}
